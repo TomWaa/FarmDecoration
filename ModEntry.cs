c9635f78a2f79e74a546dcb127458b343d1d62f5
Thomas Waaler <superxgenesis@gmail.com>
2020-03-20 18:32:15 +0100

    Repo setup
    Setting up GitHub for this project

---
+++ ModEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace FarmDecoration
+{
+    public class ModEntry : Mod
+    {
+        public override void ModEntry(IModHelper helper)
+        {
+        }
+    }
+}
